84337198dc24a5ce1bd8ab8a2eb62cd40bb93066
zaenal-blip <arif4321456789@gmail.com>
2026-01-15 14:56:20 +0700

    feat register

--- app/routes.ts
+++ app/routes.ts
@@ -1,5 +1,6 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [index("routes/home.tsx"),
-    route("/login", "routes/login.tsx")
+    route("/login", "routes/login.tsx"),
+    route("/register", "routes/register.tsx"),
 ] satisfies RouteConfig;
